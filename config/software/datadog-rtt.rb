1edbfd4591c933b820160edc353dcfac8fbe6f51
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2016-02-03 16:55:39 -0500

    [rtt] updating RTT name to new go-metro, dependencies.

--- config/software/datadog-rtt.rb
+++ config/software/datadog-rtt.rb
@@ -1,10 +1,10 @@
-name "datadog-rtt"
+name "datadog-metro"
 default_version "last-stable"
 github_token = ENV['GITHUB_TOKEN']
 
 env = {
   "GOROOT" => "/usr/local/go",
-  "GOPATH" => "/var/cache/omnibus/src/datadog-rtt"
+  "GOPATH" => "/var/cache/omnibus/src/datadog-metro"
 }
 
 dependency "libpcap"
@@ -17,15 +17,15 @@ else
 end
 
 build do
-   ship_license "https://raw.githubusercontent.com/DataDog/dd-tcp-rtt/master/LICENSE"
-   command "mkdir -p /var/cache/omnibus/src/datadog-rtt/src/github.com/DataDog", :env => env
-   command "git clone https://#{github_token}:x-oauth-basic@github.com/DataDog/dd-tcp-rtt.git", :env => env, :cwd => "/var/cache/omnibus/src/datadog-rtt/src/github.com/DataDog"
-   command "git checkout #{default_version} && git pull", :env => env, :cwd => "/var/cache/omnibus/src/datadog-rtt/src/github.com/DataDog/dd-tcp-rtt"
-   command "#{gobin} get -v -d github.com/Sirupsen/logrus", :env => env, :cwd => "/var/cache/omnibus/src/datadog-rtt"
-   command "#{gobin} get -v -d github.com/google/gopacket", :env => env, :cwd => "/var/cache/omnibus/src/datadog-rtt"
-   command "#{gobin} get -v -d github.com/DataDog/datadog-go/statsd", :env => env, :cwd => "/var/cache/omnibus/src/datadog-rtt"
-   command "#{gobin} get -v -d gopkg.in/tomb.v2", :env => env, :cwd => "/var/cache/omnibus/src/datadog-rtt"
-   command "#{gobin} get -v -d gopkg.in/yaml.v2", :env => env, :cwd => "/var/cache/omnibus/src/datadog-rtt"
-   patch :source => "libpcap-static-link.patch", :plevel => 1, :target => "/var/cache/omnibus/src/datadog-rtt/src/github.com/google/gopacket/pcap/pcap.go"
-   command "#{gobin} build -o #{install_dir}/bin/dd-tcp-rtt github.com/DataDog/dd-tcp-rtt", :env => env, :cwd => "/var/cache/omnibus/src/datadog-rtt"
+   ship_license "https://raw.githubusercontent.com/DataDog/go-metro/master/LICENSE"
+   command "mkdir -p /var/cache/omnibus/src/datadog-metro/src/github.com/DataDog", :env => env
+   command "git clone https://#{github_token}:x-oauth-basic@github.com/DataDog/go-metro.git", :env => env, :cwd => "/var/cache/omnibus/src/datadog-metro/src/github.com/DataDog"
+   command "git checkout #{default_version} && git pull", :env => env, :cwd => "/var/cache/omnibus/src/datadog-metro/src/github.com/DataDog/go-metro"
+   command "#{gobin} get -v -d github.com/cihub/seelog", :env => env, :cwd => "/var/cache/omnibus/src/datadog-metro"
+   command "#{gobin} get -v -d github.com/google/gopacket", :env => env, :cwd => "/var/cache/omnibus/src/datadog-metro"
+   command "#{gobin} get -v -d github.com/DataDog/datadog-go/statsd", :env => env, :cwd => "/var/cache/omnibus/src/datadog-metro"
+   command "#{gobin} get -v -d gopkg.in/tomb.v2", :env => env, :cwd => "/var/cache/omnibus/src/datadog-metro"
+   command "#{gobin} get -v -d gopkg.in/yaml.v2", :env => env, :cwd => "/var/cache/omnibus/src/datadog-metro"
+   patch :source => "libpcap-static-link.patch", :plevel => 1, :target => "/var/cache/omnibus/src/datadog-metro/src/github.com/google/gopacket/pcap/pcap.go"
+   command "#{gobin} build -o #{install_dir}/bin/go-metro github.com/DataDog/go-metro", :env => env, :cwd => "/var/cache/omnibus/src/datadog-metro"
 end
