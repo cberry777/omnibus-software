921de8d0bd629f805473d86faf6352bc53c353f8
Chris Berry <cberry@homeaway.com>
2016-02-11 08:23:34 -0600

    md5 must be in lower case in omnibus. stupid

--- config/software/dill.rb
+++ config/software/dill.rb
@@ -5,7 +5,7 @@ dependency "python"
 dependency "pip"
 
 source :url => "http://danse.cacr.caltech.edu/packages/dev_danse_us/dill-0.2.5.tgz",
-       :md5 => "5B0C312F034914BE2E230457747D9B6C"
+       :md5 => "5b0c312f034914be2e230457747d9b6c"
 
 build do
   # license "http://trac.mystic.cacr.caltech.edu/project/pathos/wiki/License"
--- config/software/multiprocess.rb
+++ config/software/multiprocess.rb
@@ -6,7 +6,7 @@ dependency "pip"
 dependency "dill"
 
 source :url => "http://danse.cacr.caltech.edu/packages/dev_danse_us/multiprocess-0.70.4.tgz",
-       :md5 => "587596FFF965E66A4F474A283A72F180"
+       :md5 => "587596fff965e66a4f474a283a72f180"
 
 build do
   # license "http://trac.mystic.cacr.caltech.edu/project/pathos/wiki/License"
