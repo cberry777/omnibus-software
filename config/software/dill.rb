a7aecfcd809f412972214f688c0b5373da26f5bb
Chris Berry <cberry@homeaway.com>
2016-02-11 00:09:18 -0600

    add md5

--- config/software/dill.rb
+++ config/software/dill.rb
@@ -4,7 +4,8 @@ default_version "0.2.5"
 dependency "python"
 dependency "pip"
 
-source :url => "http://danse.cacr.caltech.edu/packages/dev_danse_us/dill-0.2.5.tgz"
+source :url => "http://danse.cacr.caltech.edu/packages/dev_danse_us/dill-0.2.5.tgz",
+       :md5 => "5B0C312F034914BE2E230457747D9B6C"
 
 build do
   # license "http://trac.mystic.cacr.caltech.edu/project/pathos/wiki/License"
--- config/software/multiprocess.rb
+++ config/software/multiprocess.rb
@@ -5,7 +5,8 @@ dependency "python"
 dependency "pip"
 dependency "dill"
 
-source :url => "http://danse.cacr.caltech.edu/packages/dev_danse_us/multiprocess-0.70.4.tgz"
+source :url => "http://danse.cacr.caltech.edu/packages/dev_danse_us/multiprocess-0.70.4.tgz",
+       :md5 => "587596FFF965E66A4F474A283A72F180"
 
 build do
   # license "http://trac.mystic.cacr.caltech.edu/project/pathos/wiki/License"
