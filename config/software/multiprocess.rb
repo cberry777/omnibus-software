3e23d672f3f09c2c39fb08585ac9c21997166acf
Chris Berry <cberry@homeaway.com>
2016-02-11 08:52:37 -0600

    ship_licence

--- config/software/multiprocess.rb
+++ config/software/multiprocess.rb
@@ -10,6 +10,6 @@ source :url => "http://danse.cacr.caltech.edu/packages/dev_danse_us/multiprocess
 
 build do
   # license "http://trac.mystic.cacr.caltech.edu/project/pathos/wiki/License"
-  ship_license "BSD"
+  ship_license "http://trac.mystic.cacr.caltech.edu/project/pathos/wiki/License"
   command "#{install_dir}/embedded/bin/pip install -I --install-option=\"--install-scripts=#{install_dir}/bin\" #{name}==#{version}"
 end
