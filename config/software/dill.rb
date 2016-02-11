94e04fecde1378ea2f10fa300ecb6817d691164c
Chris Berry <cberry@homeaway.com>
2016-02-11 09:28:27 -0600

    pip not working

--- config/software/dill.rb
+++ config/software/dill.rb
@@ -4,11 +4,12 @@ default_version "0.2.5"
 dependency "python"
 dependency "pip"
 
-source :url => "http://danse.cacr.caltech.edu/packages/dev_danse_us/dill-0.2.5.tgz",
-       :md5 => "5b0c312f034914be2e230457747d9b6c"
+#source :url => "http://danse.cacr.caltech.edu/packages/dev_danse_us/dill-0.2.5.tgz",
+#       :md5 => "5b0c312f034914be2e230457747d9b6c"
 
 build do
   # license "http://trac.mystic.cacr.caltech.edu/project/pathos/wiki/License"
   ship_license "http://trac.mystic.cacr.caltech.edu/project/pathos/wiki/License"
-  command "#{install_dir}/embedded/bin/pip install -I --install-option=\"--install-scripts=#{install_dir}/bin\" #{name}==#{version}"
+  # command "#{install_dir}/embedded/bin/pip install -I --install-option=\"--install-scripts=#{install_dir}/bin\" #{name}==#{version}"
+  command "#{install_dir}/embedded/bin/pip install -I  #{name}==#{version}"
 end
--- config/software/multiprocess.rb
+++ config/software/multiprocess.rb
@@ -5,11 +5,12 @@ dependency "python"
 dependency "pip"
 dependency "dill"
 
-source :url => "http://danse.cacr.caltech.edu/packages/dev_danse_us/multiprocess-0.70.4.tgz",
-       :md5 => "587596fff965e66a4f474a283a72f180"
+#source :url => "http://danse.cacr.caltech.edu/packages/dev_danse_us/multiprocess-0.70.4.tgz",
+#       :md5 => "587596fff965e66a4f474a283a72f180"
 
 build do
   # license "http://trac.mystic.cacr.caltech.edu/project/pathos/wiki/License"
   ship_license "http://trac.mystic.cacr.caltech.edu/project/pathos/wiki/License"
-  command "#{install_dir}/embedded/bin/pip install -I --install-option=\"--install-scripts=#{install_dir}/bin\" #{name}==#{version}"
+  # command "#{install_dir}/embedded/bin/pip install -I --install-option=\"--install-scripts=#{install_dir}/bin\" #{name}==#{version}"
+  command "#{install_dir}/embedded/bin/pip install -I  #{name}==#{version}"
 end
